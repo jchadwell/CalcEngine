dd5d879d04cb63e095d8c7bf68325491cf5ab454
Jason Chadwell <jchadwell@gmail.com>
2016-06-08 07:52:02 -0400

    Move switch statement into new class

---
+++ src/com/outatimestudios/calcengine/MathEquation.java
@@ -0,0 +1,32 @@
+package com.outatimestudios.calcengine;
+
+/**
+ * Created by Jason on 6/8/2016.
+ */
+public class MathEquation {
+    public double leftVal;
+    public double rightVal;
+    public char opCode;
+    public double result;
+
+    public void execute() {
+        switch(opCode) {
+            case 'a':
+                result = leftVal + rightVal;
+                break;
+            case 's':
+                result = leftVal - rightVal;
+                break;
+            case 'd':
+                result = rightVal != 0.0d ? leftVal / rightVal : 0.0d;
+                break;
+            case 'm':
+                result = leftVal * rightVal;
+                break;
+            default:
+                System.out.println("Error - invalid opCode");
+                result = 0.0d;
+                break;
+        }
+    }
+}
