9fb9c405b15cbbc2ea4dfce4dba483b0875df263
Jason Chadwell <jchadwell@gmail.com>
2016-06-18 10:28:30 -0400

    Add PowerOf functionality through MathProcessing interface

---
+++ src/com/outatimestudios/calcengine/PowerOf.java
@@ -0,0 +1,21 @@
+package com.outatimestudios.calcengine;
+
+/**
+ * Created by Jason on 6/18/2016.
+ */
+public class PowerOf implements MathProcessing {
+    @Override
+    public String getKeyword() {
+        return "power";
+    }
+
+    @Override
+    public char getSymbol() {
+        return '^';
+    }
+
+    @Override
+    public double doCalculation(double leftVal, double rightVal) {
+        return Math.pow(leftVal, rightVal);
+    }
+}
